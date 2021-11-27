5e88302c81c4367e08d834862bc1f16f48baf1da
Scott Provence <scott.provence@yahoo.com>
2021-11-27 13:12:24 -0500

    Header component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,19 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Hulu</title>
+        <title>Hulu 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Hulu</h1>
+      <Header />
+
+      {/* Navbar */}
+
+      {/* Results */}
     </div>
   );
 }
